7762f5b90c48449781868759e2d7e0c925074c66
Jayant Tomar <jayanttomar596@gmail.com>
2026-08-19 10:51:56 +0530

    Time: 10 ms (23.95%), Space: 11.5 MB (33%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
+    int dp[500][500] ;
+
+    int solve(vector<int>& piles , int l , int r)
+    {
+        if (l == r) return piles[l] ;
+        if (dp[l][r] != -1) return dp[l][r] ;
+        int left = piles[l] - solve(piles , l+1 , r) ;
+        int right = piles[r] - solve(piles , l , r-1) ;
+
+        return dp[l][r] = max(left , right) ;
+    }
 public:
     bool stoneGame(vector<int>& piles) {
-        return true ;
+        memset(dp , -1 , sizeof(dp)) ;
+        return solve(piles , 0 , piles.size() - 1) > 0 ;
     }
 };
