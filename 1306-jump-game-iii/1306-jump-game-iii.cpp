3372dbe3354d8787daf59d5506761d78a535b528
Jayant Tomar <jayanttomar596@gmail.com>
2026-08-22 17:38:02 +0530

    Time: 0 ms (100%), Space: 30.4 MB (86.98%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,36 +1,27 @@
 class Solution {
+    bool dfs(vector<int>& arr , int i , vector<bool>& visited)
+    {
+        int n = arr.size() ;
+
+        if (i < 0 || i >= n)
+        {
+            return false ;
+        }
+
+        if (visited[i]) return false ;
+
+        if (arr[i] == 0) return true ;
+
+        visited[i] = true ;
+
+        return dfs(arr , i+arr[i] , visited) || dfs(arr , i-arr[i] , visited) ;
+    }
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size() ;
 
         vector<bool> visited(n,false) ;
 
-        queue<int> q ;
-        q.push(start) ;
-        visited[start] = true ;
-
-        while(!q.empty())
-        {
-            int front = q.front() ;
-            q.pop() ;
-
-            if (arr[front] == 0) return true ;
-            int op1 = front + arr[front] ;
-            int op2 = front - arr[front] ;
-
-            if (op1 < n && !visited[op1])
-            {
-                q.push(op1) ;
-                visited[op1] = true ;
-            }
-
-            if (op2 >= 0 && !visited[op2])
-            {
-                q.push(op2) ;
-                visited[op2] = true ;
-            }
-        }
-
-        return false ;
+        return dfs(arr , start , visited) ;
     } 
 };
