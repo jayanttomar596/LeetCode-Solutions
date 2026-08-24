96f1ed6dc2b569d894cb388c4cc9125020064396
Jayant Tomar <jayanttomar596@gmail.com>
2026-08-24 22:49:27 +0530

    Time: 295 ms (5.11%), Space: 101.3 MB (5.11%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,31 +1,48 @@
 class Solution {
-public:
-    bool canReach(string s, int minJump, int maxJump) {
-        int n = s.size() ;
+    vector<int> dp;
+    set<int> unvisited;
 
-        vector<bool> dp(n , false) ;
-        dp[0] = true ;
+    bool solve(string &s, int minJump, int maxJump, int index) {
+        if (index == s.size() - 1)
+            return true;
 
-        int reachableCount = 0 ;
+        if (dp[index] != -1)
+            return dp[index];
 
-        for (int i = 1 ; i < n ; i++)
-        {
-            if (i - minJump >= 0 && dp[i - minJump])
-            {
-                reachableCount++ ;
-            }
+        int left = index + minJump;
+        int right = min(index + maxJump, (int)s.size() - 1);
 
-            if (i - maxJump - 1 >= 0 && dp[i - maxJump - 1])
-            {
-                reachableCount-- ;
-            }
+        auto it = unvisited.lower_bound(left);
 
-            if (s[i] == '0' && reachableCount > 0)
-            {
-                dp[i] = true ;
-            }
+        while (it != unvisited.end() && *it <= right) {
+            int next = *it;
+
+            // Erase BEFORE recursion.
+            unvisited.erase(it);
+
+            if (solve(s, minJump, maxJump, next))
+                return dp[index] = 1;
+
+            // Recursive call may have modified the set,
+            // so NEVER use 'it' again.
+            it = unvisited.lower_bound(next + 1);
         }
 
-        return dp[n-1] ;
+        return dp[index] = 0;
+    }
+
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        dp.assign(n, -1);
+        unvisited.clear();
+
+        for (int i = 1; i < n; i++) {
+            if (s[i] == '0')
+                unvisited.insert(i);
+        }
+
+        return solve(s, minJump, maxJump, 0);
     }
 };
