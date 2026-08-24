866748735aabfd12299cbe1940e24dc3499faedf
Jayant Tomar <jayanttomar596@gmail.com>
2026-08-24 22:57:13 +0530

    Time: 3 ms (94.89%), Space: 20.1 MB (69.03%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,48 +1,22 @@
 class Solution {
-    vector<int> dp;
-    set<int> unvisited;
-
-    bool solve(string &s, int minJump, int maxJump, int index) {
-        if (index == s.size() - 1)
-            return true;
-
-        if (dp[index] != -1)
-            return dp[index];
-
-        int left = index + minJump;
-        int right = min(index + maxJump, (int)s.size() - 1);
-
-        auto it = unvisited.lower_bound(left);
-
-        while (it != unvisited.end() && *it <= right) {
-            int next = *it;
-
-            // Erase BEFORE recursion.
-            unvisited.erase(it);
-
-            if (solve(s, minJump, maxJump, next))
-                return dp[index] = 1;
-
-            // Recursive call may have modified the set,
-            // so NEVER use 'it' again.
-            it = unvisited.lower_bound(next + 1);
-        }
-
-        return dp[index] = 0;
-    }
-
 public:
     bool canReach(string s, int minJump, int maxJump) {
-        int n = s.size();
+        int n = s.size() ;
 
-        dp.assign(n, -1);
-        unvisited.clear();
+        vector<bool> dp(n ,false) ;
+        dp[0] = true ;
 
-        for (int i = 1; i < n; i++) {
-            if (s[i] == '0')
-                unvisited.insert(i);
+        int reachableCount = 0 ;
+
+        for (int i = 1 ; i < n ; i++)
+        {
+            if (i - minJump >= 0 && dp[i-minJump]) reachableCount++ ;
+
+            if (i - maxJump - 1 >= 0 && dp[i-maxJump-1]) reachableCount-- ;
+
+            if (s[i] == '0' && reachableCount > 0) dp[i] = true ;
         }
 
-        return solve(s, minJump, maxJump, 0);
+        return dp[n-1] ;
     }
 };
